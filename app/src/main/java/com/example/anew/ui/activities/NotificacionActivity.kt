16769af076af0e9b2c28c84b08f4111a6532dd48
AlexCondor <alexguerron@gmail.com>
2023-07-29 08:55:42 -0500

    notificaciones (falta textos big/small y la importancia)

--- app/src/main/java/com/example/anew/ui/activities/NotificacionActivity.kt
+++ app/src/main/java/com/example/anew/ui/activities/NotificacionActivity.kt
@@ -1,7 +1,14 @@
 package com.example.anew.ui.activities
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.anew.R
 import com.example.anew.databinding.ActivityNotificacionBinding
 
@@ -11,7 +18,55 @@ class NotificacionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ActivityNotificacionBinding.inflate(layoutInflater)
+        binding=ActivityNotificacionBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnNotificacion.setOnClickListener{
+            createNotificationChannel()
+            sendNorificacion()
+        }
+    }
+
+
+    val CHANNEL = "Notificaciones"
+
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Variedades"
+            val descriptionText = "notificaciones simple de variedades"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    fun sendNorificacion(){
+
+        val noti = NotificationCompat.Builder(this, CHANNEL)
+        //cuerpo de Notificacion
+        noti.setContentTitle("Titulo")
+        noti.setContentText("No te olvides de parpadear ni de tomar awita <3")
+        noti.setSmallIcon(R.drawable.logo2)
+        //prioridad de la notificacion
+        noti.setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        noti.setStyle(
+            NotificationCompat.BigTextStyle()
+                .bigText("Recordatorio awita de coco... texto grande .... ..... gracias")
+        )
+        //eniar la notificacion
+        with(NotificationManagerCompat.from(this)){
+            notify(1,noti.build())
+        }
+
     }
 }
